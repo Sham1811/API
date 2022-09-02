bedda0d272b9f7fac6b11dca73b5911a1ce36047
Sham1811 <sathish.sham18@gmail.com>
2022-09-02 20:44:42 +0530

    Hi

---
+++ src/org/company/Company.java
@@ -0,0 +1,20 @@
+package org.company;
+
+import org.client.Client;
+
+public class Company extends Client {
+	
+	private void companyName() {
+		
+		System.out.println("company name TCS");
+	}
+	
+	public static void main(String[] args) {
+		
+		Company co = new Company();
+		co.companyName();
+		co.clientName();
+		
+	}
+
+}
