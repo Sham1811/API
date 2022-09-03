9984f794007bf8d52bb2fda3e092cd6d71026d2c
Sham1811 <sathish.sham18@gmail.com>
2022-09-03 13:15:09 +0530

    sunny

--- src/org/client/Client.java
+++ src/org/client/Client.java
@@ -5,6 +5,9 @@ public class Client {
 	public void clientName() {
 		
 		System.out.println("client name Charu");
+		System.out.println("client name Charu");
+		System.out.println("client name Charu");
+		System.out.println("client name Charu");
 	}
 
 }
